6280da729596d91a5f09ba97bb899cc5519d3da2
sere siaka <seresiaka@gmail.com>
2023-11-16 09:25:01 +0100

    installation dees

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> Ser
+          Edit <code>src/App.jsx</code> Se
         </p>
       </div>
       <p className="read-the-docs">
